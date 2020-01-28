5b5fb978ab5587f7bcdd81f606ac5e0ebe34d3e0
openaibot <mr.kaka1234@bk.ru>
2020-01-28 21:12:32 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Боти
+          123123123
         </p>
         <a
           className="App-link"
